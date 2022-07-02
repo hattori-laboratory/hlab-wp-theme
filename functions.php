afe61653b541e293084a85b866319a0e5521e937
yuzuki-chi <47371712+yuzuki-chi@users.noreply.github.com>
2022-07-02 15:45:29 +0900

    Authorページの新規作成

--- functions.php
+++ functions.php
@@ -1,7 +1,27 @@
 <?php
 register_nav_menus(
-			array(
-				'primary' => 'Header navigation',
-				'footer'  => 'footer-nav',
-			)
-		);
+	array(
+		'primary' => 'Header navigation',
+		'footer'  => 'footer-nav',
+	)
+);
+
+/**
+ * update_profile_fields
+ * ユーザ情報にカスタムカラムを追加する
+ */
+function update_profile_fields($contactmethods)
+{
+	//項目の削除
+	unset($contactmethods['aim']);
+	unset($contactmethods['jabber']);
+	unset($contactmethods['yim']);
+	//項目の追加
+	$contactmethods['twitter'] = 'Twitter';
+	$contactmethods['facebook'] = 'Facebook';
+	$contactmethods['github'] = 'GitHub';
+	$contactmethods['qiita'] = 'Qiita';
+
+	return $contactmethods;
+}
+add_filter('user_contactmethods', 'update_profile_fields', 10, 1);
